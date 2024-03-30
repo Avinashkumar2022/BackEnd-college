49839a0f8653113893b3e444a3d899ef7cfac278
Avinash <avinashkumar200217@gmail.com>
2024-03-30 14:06:09 +0530

    zlib

--- queryString.js
+++ queryString.js
@@ -4,7 +4,22 @@ const url=require("url");
 
 const server=http.createServer((req,res)=>
 {
-
+    let parsedURL=url.parse(req.url,true);
+    console.log(parsedURL);
+    console.log(parsedURL.query.name);
+    if(parsedURL.pathname=='/')
+    {
+        let readableStream=fs.createReadStream('public/index.html');
+        readableStream.pipe(res);
+    }
+    else if(parsedURL.pathname=='/submit' && req.method=='GET')
+    {
+        let writableStream=fs.createWriteStream('form_data.txt');
+        let query =parsedURL.query;
+        writableStream.write(query.name+'\n');
+        writableStream.write(query.email+'\n');
+        writableStream.on('finish')
+    }
 })
 server.listen(5000,()=>
 {
---
+++ zlib.js
@@ -0,0 +1,34 @@
+/* 
+How can you create a node.js server that serves a specific text file, compresses it with gzip encoding, and dynamically responds to http requests? Provide a detaield code solution
+*/
+
+const fs= require('fs');
+const http= require('http');
+const zlib=require('zlib');
+
+const server= http.createServer((res,req)=>
+{
+    
+    const ws=fs.createWriteStream("Comp.txt");
+    ws.write("Hello world!!");
+    const rs=fs.createReadStream("Comp.txt","utf-8");
+    if(req.url=="/comp")
+    {
+        rs.on("data",(chunk)=>
+        {
+            res.end(chunk);
+        })
+        zlib.gzip("Comp.txt", (err, compressed));
+    }
+    else if(req.url=="/decomp")
+    {
+        zlib.gunzip("comp.gz",(err,decomposed)=>
+        {
+            console.log(decomposed);
+            // res.end(decomposed.toString());
+        })
+    }
+    else{
+        res.end("file not found or no method")
+    }
+})
