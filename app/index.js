63d4a30894aa6c26a3a73f5f16419abdea734455
Avinash <avinashkumar200217@gmail.com>
2024-04-03 15:21:09 +0530

    updated index.js file

--- app/index.js
+++ app/index.js
@@ -1,2 +1,25 @@
-const exp=require('express');
-const app=express();
+const express = require('express'); // Import Express module
+const app = express(); // Initialize Express application
+
+const fs = require("fs");
+
+app.get("/", (req, res) => {
+    fs.writeFile('student.txt', "Hello from the other side", (err, data) => {
+        if (err) {
+            res.status(404).send("File not found");
+        } else {
+            console.log(data);
+            fs.readFile("student.txt", "utf-8", (err, data) => {
+                if (err) {
+                    res.status(404).send("File not found");
+                } else {
+                    res.download("student.txt");
+                }
+            });
+        }
+    });
+});
+
+app.listen(1000, () => {
+    console.log("Server Started");
+});
